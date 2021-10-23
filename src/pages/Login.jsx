70fd509d427ff7b4bcab63b0b757de766d56ec22
Joe Cillo <JCillo507@yahoo.com>
2021-10-23 14:32:11 -0400

    login page built

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,72 @@
-import {useState, useContext, useEffect} from 'react'
-import { useHistory} from 'react-router-dom'
-import FirebaseContext from '../context/firebase'
+import { useState, useContext, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
+import FirebaseContext from "../context/firebase";
 
 const Login = () => {
-  const history = useHistory()
-  const {firebase} =useContext(FirebaseContext)
-  const [emailAddess, setEmailAddess] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
-  const isInvalid = password === ''||emailAddess ===""
-  const handleLogin = ()=>{
-
-  }
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || emailAddress === "";
+  const handleLogin = () => {};
   useEffect(() => {
-   document.title="Login - Instagram"
-  }, [])
+    document.title = "Login - Instagram";
+  }, []);
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      njkm
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iphone " />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img
+              src="/images/logo.png"
+              alt="Instagram"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+              value={emailAddress}
+            />
+            <input
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              type="text"
+              aria-label="Enter your Password"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-500 text-white w-full rounded h-8 font-bold
+            ${isInvalid && "opacity-50"}`}
+            >
+              Login
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+          <p className="text-sm">
+            Don't have an account?{` `}
+            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
