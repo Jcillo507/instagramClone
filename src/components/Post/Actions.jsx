38319bebb2f603e41ef6fa9ced4261c63a2005c8
Joe Cillo <JCillo507@yahoo.com>
2021-10-27 18:06:38 -0400

    can like photo

--- src/components/Post/Actions.jsx
+++ src/components/Post/Actions.jsx
@@ -9,7 +9,59 @@ const Actions = ({ docId, totalLikes, likedPhoto, handleFocus }) => {
    const [likes, setLikes] = useState(totalLikes);
    const { firebase, FieldValue } = useContext(FirebaseContext);
    
-  return <div></div>;
+   const handleToggleLike = async ()=>{
+        setToggleLiked((toggleLiked) => !toggleLiked);
+
+        await firebase
+          .firestore()
+          .collection("photos")
+          .doc(docId)
+          .update({
+            likes: toggleLiked
+              ? FieldValue.arrayRemove(userId)
+              : FieldValue.arrayUnion(userId),
+          });
+
+        setLikes((likes) => (toggleLiked ? likes - 1 : likes + 1));
+   }
+  return (
+    <>
+      <div className="flex justify-between p-4">
+        <div className="flex">
+          <svg
+            onClick={handleToggleLike}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                handleToggleLike();
+              }
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            tabIndex={0}
+            className={`w-8 mr-4 select-none cursor-pointer focus:outline-none ${
+              toggleLiked ? "fill-red text-red-primary" : "text-black-light"
+            }`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            />
+          </svg>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Actions;
+
+Actions.propTypes = {
+  docId: PropTypes.string.isRequired,
+  totalLikes: PropTypes.number.isRequired,
+  likedPhoto: PropTypes.bool.isRequired,
+  handleFocus: PropTypes.func.isRequired,
+};
